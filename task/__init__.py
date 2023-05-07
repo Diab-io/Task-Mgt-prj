4910b7a67525e8e0e7b5309450523c34b86d0ca3
Odimayo David <odimayodavid7@gmail.com>
2023-05-07 14:24:55 +0100

    Inplemented the login manager

--- task/__init__.py
+++ task/__init__.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_login import LoginManager
 from dotenv import load_dotenv
 
 DB_NAME = 'database.db'
@@ -16,12 +17,21 @@ def create_app():
     from .views import view
     from .auth import auths
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auths.login'
+
     from .models import User, Todo
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     create_database(app)
     
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(auths, url_prefix='/')
+
 
     return app
 
