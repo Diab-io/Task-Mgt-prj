824aab65a920fc33a84c4add5ee151299c7e0883
Odimayo David <odimayodavid7@gmail.com>
2023-05-07 14:25:29 +0100

    Authenticated user in the login route

--- task/auth.py
+++ task/auth.py
@@ -1,14 +1,28 @@
 import re
-from flask import Flask, render_template, Blueprint, flash, redirect,url_for
+from flask import render_template, Blueprint, flash, redirect, url_for
 from flask import request
 from .models import User
 from task import db
+from flask_login import login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auths = Blueprint('auths', __name__)
 
-@auths.route('/login')
+@auths.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            pwd_check = check_password_hash(user.password, password)
+            if pwd_check:
+                login_user(user)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Wrong password', category='error')
+        else:
+            flash('User not found')
     return render_template('login.html')
 
 @auths.route('/signup', methods=['GET', 'POST'])
@@ -19,19 +33,26 @@ def signup():
     password = request.form.get('password')
     confpass = request.form.get('confirm-password')
 
-    user_exists = User.query.filter_by(email=email).first()
-    if user_exists:
-        flash('User already exists', category='error')
-    elif password != confpass:
-        flash('Please confirm your password', category='error')
-    elif email:
-        if not re.fullmatch(email_regex, email):
-            flash('Enter a valid email address', category='error')
-    elif len(name) <  4:
-        flash('Name is too short', category='error')
-    else:
-        new_user = User(name=name, email=email, password=generate_password_hash(password, 'sha256'))
-        db.session.add(new_user)
-        db.commit()
-        redirect(url_for('auths.login'))
+    if request.method == 'POST':
+        user_exists = User.query.filter_by(email=email).first()
+        if user_exists:
+            flash('User already exists', category='error')
+        elif password != confpass:
+            flash('Please confirm your password', category='error')
+        elif email:
+            if not re.fullmatch(email_regex, email):
+                flash('Enter a valid email address', category='error')
+        elif len(name) <  4:
+            flash('Name is too short', category='error')
+        else:
+            new_user = User(name=name, email=email, password=generate_password_hash(password, 'sha256'))
+            db.session.add(new_user)
+            db.commit()
+            redirect(url_for('auths.login'))
     return render_template('signup.html')
+
+@auths.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    redirect(url_for('auths.login'))
